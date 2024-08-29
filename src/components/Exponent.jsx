189927f1ae889c7e51114d426c1f6e01d4ab218e
Reda Dahmane <r.dahmanee@gmail.com>
2024-08-29 15:42:50 +0200

    done

--- src/components/Exponent.jsx
+++ src/components/Exponent.jsx
@@ -1,7 +1,15 @@
-import React from "react";
-
-function Exponent({ count }) {
-  return <div></div>;
+function Exponent({ num, exponent }) {
+  return (
+    <div className="exponent-counter-container">
+      <p className="exponent-label">
+        {num}
+        <sup>{exponent}</sup>
+      </p>
+      <p className="exponent-result">
+        {Array(exponent).fill(num).join(" * ")} ={" "}
+        <span className="total">{num ** exponent}</span>
+      </p>
+    </div>
+  );
 }
-
 export default Exponent;
